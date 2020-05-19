59f116a69f227234f82a39053bb2fcdadccd5989
Heladio-ac <heladio.ac@gmail.com>
2020-05-19 17:36:43 -0500

    Agregar descarga del archivo summary, necesario para el preprocesamiento

--- get_data.py
+++ get_data.py
@@ -1,7 +1,10 @@
 import os
 
+_record_download_command = 'gsutil -m cp -r gs://chbmit-1.0.0.physionet.org/{case}/{record} data/{case}/'
+
+_summary_download_command = 'gsutil -m cp -r gs://chbmit-1.0.0.physionet.org/{case}/{case}-summary.txt data/{case}/'
+
 _data_subpath = 'data'
-_command = 'gsutil -m cp -r gs://chbmit-1.0.0.physionet.org/{case}/{record} data/{case}/'
 _cwd = os.getcwd()
 _data_path = os.path.join(_cwd, _data_subpath)
 
@@ -9,11 +12,19 @@ _download_tries = 5
 
 
 def make_dirs():
+    ''' Create the directories required to locally store the EEG data
+        as well as download the summary file for each case
+    '''
     for i in range(1, 25):
         num = '0' + str(i) if i < 10 else str(i)
-        case_subpath = f'chb{num}'
-        case_path = os.path.join(_data_path, case_subpath)
+        case = f'chb{num}'
+        case_path = os.path.join(_data_path, case)
         os.makedirs(case_path, exist_ok=True)
+
+        file_path = os.path.join(case_path, f'{case}-summary.txt')
+        if not os.path.isfile(file_path):
+            print('Downloading summary file for case: ', case)
+            result = os.system(_summary_download_command.format(case=case))
 
 
 def main():
@@ -42,7 +53,8 @@ def main():
         while result != 0:
             if tries < _download_tries:
                 tries += 1
-                result = os.system(_command.format(case=case, record=record))
+                result = os.system(
+                    _record_download_command.format(case=case, record=record))
             else:
                 print('Skiping record: ', record)
                 print('Record took too many tries to download')
